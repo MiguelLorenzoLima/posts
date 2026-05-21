33e840d244a2df085ec2e55de6cdc5e53a64ef81
MiguelLorenzoLima <miguel.lorenzo.lb@gmail.com>
2026-05-20 23:02:41 -0300

    add component container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,30 @@
+import { Container } from '@/components/container';
 import { PostsList } from '@/components/PostsList/index';
 import { SpinLoader } from '@/components/SpinLoader';
 import { Suspense } from 'react';
 
 export default async function HomePage() {
   return (
-    <div className='text-slate-900 bg-slate-100 min-h-screen dark:bg-slate-900 dark:text-slate-100'>
+    <Container>
       <header>
         <h1 className='text-6xl font-bold text-center py-8'>Liluiaaaa</h1>
+        <p className='text-justify'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum,
+          praesentium possimus veniam nisi totam repellat ut obcaecati,
+          consequuntur error explicabo impedit rem necessitatibus autem dolores.
+          Laudantium deleniti iste nulla veritatis? Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Laborum, praesentium possimus veniam
+          nisi totam repellat ut obcaecati, consequuntur error explicabo impedit
+          rem necessitatibus autem dolores. Laudantium deleniti iste nulla
+          veritatis? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Laborum, praesentium possimus veniam nisi totam repellat ut obcaecati,
+          consequuntur error explicabo impedit rem necessitatibus autem dolores.
+          Laudantium deleniti iste nulla veritatis? Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Laborum, praesentium possimus veniam
+          nisi totam repellat ut obcaecati, consequuntur error explicabo impedit
+          rem necessitatibus autem dolores. Laudantium deleniti iste nulla
+          veritatis?
+        </p>
       </header>
 
       <Suspense fallback={<SpinLoader />}>
@@ -16,6 +34,6 @@ export default async function HomePage() {
       <footer>
         <p className='text-6xl font-bold text-center py-8'>Foooter</p>
       </footer>
-    </div>
+    </Container>
   );
 }
---
+++ src/components/container/index.tsx
@@ -0,0 +1,10 @@
+type Props = {
+  children: React.ReactNode;
+};
+export function Container({ children }: { children: React.ReactNode }) {
+  return (
+    <div className='text-slate-950 bg-slate-100 min-h-screen '>
+      <div className=' max-w-screen-lg mx-auto px-8'>{children}</div>
+    </div>
+  );
+}
