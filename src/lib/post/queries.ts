fc88ee99ac46e47a17ce00644f4c3f133bc97cc2
MiguelLorenzoLima <miguel.lorenzo.lb@gmail.com>
2026-05-28 17:06:52 -0300

    add update postBySlugCached

--- src/lib/post/queries.ts
+++ src/lib/post/queries.ts
@@ -5,9 +5,12 @@ export const findAllPublicPostsCached = cache(
   async () => await postRepository.findAllPublic(),
 );
 
-export const findPostBySlugCached = cache(
-  async (slug: string) => await postRepository.findBySlug(slug),
-);
+export const findPostBySlugCached = cache(async (slug: string) => {
+  const post = await postRepository.findBySlug(slug).catch(() => undefined);
+  if (!post) throw new Error(`Post with slug "${slug}" not found`);
+
+  return post;
+});
 
 export const findPostByIdCached = cache(
   async (id: string) => await postRepository.findById(id),
